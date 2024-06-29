19f2f086f45a37595ab53575932035193c6eb790
KaiCode <kaiquemedeiros440@gmail.com>
2024-06-29 12:16:09 -0300

    toBeCloseTo() Jest function

--- __TESTS__/grade.test.js
+++ __TESTS__/grade.test.js
@@ -1,10 +1,7 @@
 const average = require("../grade.js")
 
-describe("Checking student grades",()=>{
-    test("shoud check if the average of the grades 4, 3 and 8 is less than 6", ()=>{
-        expect(average(4, 3, 8)).toBeLessThan(6)
-    })
-    test("shoud check if the average of the grades 6, 6 and 5 is less than or equal to 6", ()=>{
-        expect(average(6, 6, 6)).toBeLessThanOrEqual(6)
+describe("checking students grade",()=>{
+    test("should check if the average of the grades 4, 3 and 6 is equal to 4.3", ()=>{
+        expect(average(4, 3, 6)).toBeCloseTo(4.33, 1)
     })
 })

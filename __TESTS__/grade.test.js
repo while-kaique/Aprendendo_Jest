a9d016056c624f9e2a86bf5ea66d9749de11d85c
KaiCode <kaiquemedeiros440@gmail.com>
2024-06-29 11:52:47 -0300

    toBeLessThan() and toBeLessThanOrEqual() Jest functions

--- __TESTS__/grade.test.js
+++ __TESTS__/grade.test.js
@@ -1,10 +1,10 @@
 const average = require("../grade.js")
 
 describe("Checking student grades",()=>{
-    test("shoud check if the average of the grades 7, 7 and 8 is greater than 6", ()=>{
-        expect(average(7, 7, 8)).toBeGreaterThan(6)
+    test("shoud check if the average of the grades 4, 3 and 8 is less than 6", ()=>{
+        expect(average(4, 3, 8)).toBeLessThan(6)
     })
-    test("shoud check if the average of the grades 6, 6 and 6 is greater than or equal to 6", ()=>{
-        expect(average(6, 6, 6)).toBeGreaterThanOrEqual(6)
+    test("shoud check if the average of the grades 6, 6 and 5 is less than or equal to 6", ()=>{
+        expect(average(6, 6, 6)).toBeLessThanOrEqual(6)
     })
 })
